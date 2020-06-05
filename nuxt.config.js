a839c47eacb13b61181c81eb1da5f143f2438193
Donaven Snowden <donaven.snowden@gmail.com>
2020-06-05 14:23:46 -0700

    inital commit

---
+++ nuxt.config.js
@@ -0,0 +1,71 @@
+export default {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'Prismic + Nuxt Blog example',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Prismic + Nuxt Blog example' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lato:300,400,700,900' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@/assets/css/resetr.css',
+    '@/assets/css/common.css'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // modules for full static before `nuxt export` (coming in v2.12)
+    '@/modules/static',
+    '@/modules/crawler',
+    // https://prismic-nuxt.js.org/
+    '@nuxtjs/prismic'
+  ],
+
+  prismic: {
+    endpoint: 'https://thepotentialadventures.cdn.prismic.io/api/v2',
+    linkResolver: '@/plugins/link-resolver',
+    htmlSerializer: '@/plugins/html-serializer',
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      config.resolve.alias['vue'] = 'vue/dist/vue.common'
+    }
+  },
+
+  generate: {
+    fallback: '404.html' // Netlify reads a 404.html, Nuxt will load as an SPA
+  }
+}
